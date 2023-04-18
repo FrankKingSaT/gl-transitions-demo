022b45fc3303572e1e1e6b6334ceeab86d7c8148
付金祥 <fujinxiang@cvte.com>
2023-04-18 16:47:52 +0800

    remove fileSelect

--- src/App.js
+++ src/App.js
@@ -4,43 +4,19 @@ import drawTranstion from "./transition";
 import "./App.css";
 
 function App() {
-
-  function onSelectImageFrom(e) {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = async (e) => {
-      // 下面写一段代码，将 e.target.result 赋值给 imgFrom 的 src
-      const imgFrom = document.getElementById("imgFrom");
-      imgFrom.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
-  }
-
-  function onSelectImageTo(e) {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = async (e) => {
-      const imgTo = document.getElementById("imgTo");
-      imgTo.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
-  }
-
   function startShow() {
     const container = document.getElementById("transitions-container");
-    const imgFrom = document.getElementById("imgFrom");
-    const imgTo = document.getElementById("imgTo");
+    const from = document.getElementById("imgFrom");
+    const to = document.getElementById("imgTo");
     const select = document.getElementById("transtions-select");
     const transitionName = select.value;
-    drawTranstion(container, imgFrom, imgTo, 356, 200,transitionName);
+    drawTranstion(container, from, to, 356, 200, transitionName);
   }
 
   return (
     <div className="App">
       <div id='transitions-container'></div>
       <div id='operations'>
-        {/* <input id="imageFrom" type="file" onChange={onSelectImageFrom} />
-        <input id="imageTo" type="file" onChange={onSelectImageTo} /> */}
         <select id="transtions-select">
           {transitions.map((t) => {
             return <option value={t.name}>{t.name}</option>;
@@ -48,7 +24,7 @@ function App() {
         </select>
         <button onClick={startShow}>add transition</button>
         <div>
-          <img id="imgFrom" src='/imageFrom.jpg' />
+        <img id="imgFrom" src='/imageFrom.jpg' />
           <img id="imgTo" src='/imageTo.jpg' />
         </div>
       </div>
